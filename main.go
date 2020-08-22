a682e69296374a4aed737d533b894723d1f3641d
Ade Yusuf Yaumul Isnain <yusuf@passionfood.id>
2020-08-22 20:43:38 +0700

    Setup access log file

--- main.go
+++ main.go
@@ -2,21 +2,47 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
+	"path"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
+	var (
+		err     error
+		logFile *os.File
+		logPath string
+	)
+
+	err = godotenv.Load()
 
 	if err != nil {
 		log.Fatal("Error loading .env file")
 		os.Exit(1)
 	}
+
+	logPath, err = os.Getwd()
+
+	if err != nil {
+		log.Fatal("Error getting working directory")
+		os.Exit(1)
+	}
+
+	fileName := path.Join(logPath, "logs/access.log")
+
+	logFile, err = os.OpenFile(fileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+
+	if err != nil {
+		log.Fatal("Failed to write/create log file")
+		os.Exit(1)
+	}
+
+	gin.DefaultWriter = io.MultiWriter(logFile, os.Stdout)
 
 	router := gin.Default()
 
